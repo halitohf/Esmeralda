80fe52b1bdca81e8d38cbafeb80083ef6872461c
Jose de Jesus Zaragoza <halitohf@live.com>
2023-06-17 22:23:49 -0600

    Add Hearth

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,17 @@ export default function FirstPost() {
     return (
         <>
         <Head>
-            <title>Inicio</title>
+            <title>Inicio Pagina </title>
             <link rel="icon" href='/images/Esme.png'/>
         </Head>
         <h1 className='title'>
-            Bby <Link href="/posts/first-post">I love You 💗</Link></h1>
+          Bby <Link href="/posts/first-post">I love You 💘</Link></h1>
     <div>
       <Image
         src="/images/Dan.jpg"
         width={300}
-        height={520}
-        alt="Picture of the author"
+        height={500}
+        alt="Imagen"
       />
     </div>
         </>
